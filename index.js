b889a42e4cbe48a72acce49d13bad8b2cc51afb3
Ngo <dinh.minh.tue.ngo@accenture.com>
2019-09-24 14:20:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const port = process.env.PORT || 8081;
+
+const posts = require("./route/api/posts");
+
+app.use("/api/posts", posts);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
---
+++ route/api/posts.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+router.post("/", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+
+  res.status(201).send();
+});
+
+router.delete("/:id", async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    "mongodb://localhost:27017",
+    {
+      useNewUrlParser: true
+    }
+  );
+
+  return client.db("heejin_closet").collection("posts");
+}
+
+module.exports = router;
