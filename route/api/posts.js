8dcb0d8acaf407c28aa0e4747d62782bca446f31
Ngo <dinh.minh.tue.ngo@accenture.com>
2019-09-25 14:52:38 +0200

    test api 2

--- route/api/posts.js
+++ route/api/posts.js
@@ -4,7 +4,7 @@ const mongodb = require("mongodb");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const posts = await loadPostsCollection();
+  // const posts = await loadPostsCollection();
   res.send("I love you Heejin");
   // res.send(await posts.find({}).toArray());
 });
